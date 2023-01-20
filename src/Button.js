2f5bd1304bf9f1747f312b7eb65143c476ad7fdb
naveendoddi <prasadnaveen847@gmail.com>
2023-01-20 11:48:06 +0530

    thank God

--- src/Button.js
+++ src/Button.js
@@ -14,6 +14,7 @@ function Toggle(){
         </button>
         <button onClick={()=> setColor("voilet")}>
             voilet
+            {/* thankyougod */}
         </button>
         </>
     )
