21fb18f95a6f8f23e45a03e26d193e46a14bf703
naveendoddi <prasadnaveen847@gmail.com>
2023-11-01 23:34:35 +0530

    kdjf

---
+++ src/arthmeticOperations.js
@@ -0,0 +1,28 @@
+function Addition(arg1,arg2){
+    return arg1 + arg2
+}
+
+function Subtration(arg1,arg2){
+    return arg1 - arg2
+}
+
+function Multiplication(arg1,arg2){
+    
+    // document.getElementById("output").innerText = arg1 * arg2
+    // return(
+    //     <>
+    //     Multiplication
+    //     {arg1 * arg2}
+    //     </>    
+    // )
+}
+
+function Division(arg1,arg2){
+    return arg1 / arg2
+}
+
+function Persantile(arg1,arg2){
+    return arg1 % arg2
+}
+
+export {Addition, Subtration, Multiplication, Division, Persantile}
--- src/calculator.js
+++ src/calculator.js
@@ -1,32 +1,45 @@
 import React from "react";
-
-
+import { Addition } from "./arthmeticOperations";
+import { Subtration, Multiplication, Division, Persantile } from "./arthmeticOperations";
+import { useState } from "react";
+import Toggle from './Button.js'
 
 function Cal(){
+    var [operation , setOperation] = useState("")
+
     function calculate(){
-        var input = document.getElementById("input").value
-        console.log(input)
-        
-        document.getElementById("output").innerText = eval(input)
+        if(operation == "sub"){
+            console.log("sub")
+        }
+    }
+    
+    function sum(){
+        var input = document.getElementById("input").innerText
+
+        document.getElementById("input").innerText = input + "+"
     }
     function write(click){
-        // console.log(click.target.innerText)
+       
         var number = click.target.innerText
-        var input = document.getElementById("input").value
+        var input = document.getElementById("input").innerText
         input = input + number
-        input = Number(input)
-        document.getElementById("input").value = input
-        calculate()
+        
+        document.getElementById("input").innerText = input
+
+        document.getElementById("output").innerText = eval(input)
+        
+        // calculate(input)
     }
     return(
         <>
-            <p>the</p>
+
             <div id="maindiv">
-                <input id="input" onChange={calculate}/>
-                {/* <p id="input" ></p> */}
-                <p id="output">=</p>
+            
+                <p id="input">  </p>
+                <p id="output">  </p>
 
                 <table>
+
                     <tr>
                         <td><button onclick="ac()">AC</button></td>
                         <td><button onclick="per()">per%</button></td>
@@ -37,24 +50,24 @@ function Cal(){
                         <td onClick={write}>7</td>
                         <td onClick={write}>8</td>
                         <td onClick={write}>9</td>
-                        <td><button onclick="mul()">mul*</button></td>
+                        <td><button onClick={Multiplication(23,3)}>mul*</button></td>
                     </tr>
                     <tr>
-                        <td>4</td>
-                        <td>5</td>
-                        <td>6</td>
-                        <td><button id="sub" onclick="sub()">sub-</button></td>
+                        <td onClick={write}>4</td>
+                        <td onClick={write}>5</td>
+                        <td onClick={write}>6</td>
+                        <td><button id="sub" onClick={operation = "sub"}>sub-</button></td>
                     </tr>
                     <tr>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td><button id="sum" onclick="sum()">sum+</button></td>
+                        <td onClick={write}>1</td>
+                        <td onClick={write}>2</td>
+                        <td onClick={write}>3</td>
+                        <td><button id="sum" onClick={sum}>sum+</button></td>
                     </tr>
                     <tr>
-                        <td>.</td>
-                        <td>0</td>
-                        <td>00</td>
+                        <td onClick={write}>.</td>
+                        <td onClick={write}>0</td>
+                        <td onClick={write}>00</td>
                         <td><button onclick="ans()">=</button></td>   
                     </tr>
                 </table>
